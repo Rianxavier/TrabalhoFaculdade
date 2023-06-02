a8b307553dacbe8f66c19497cb9b8fd250b3aba8
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2023-06-02 14:37:49 -0300

    Update cadastro.js

--- Assets/JS/cadastro.js
+++ Assets/JS/cadastro.js
@@ -6,7 +6,7 @@ let btnVerConfSenha = document.querySelector('#verConfSenha');
 const formulario = document.querySelector('.formulario');
 
 let inputNome = document.querySelector('#nome');
-let inputDataNacimento = document.querySelector('#nasc');
+let inputDataNascimento = document.querySelector('#nasc');
 let inputCpf = document.querySelector('#cpf');
 let inputCelular = document.querySelector('#number');
 let inputEmail = document.querySelector('#email');
@@ -183,7 +183,7 @@ formulario.addEventListener('submit', function(event) {
 
 
     
-    if (!senhaVerifica || !cpfVerifica || inputNome.length === 0 || inputDataNacimento.length === 0 || inputEmail.length === 0 || inputSenha.length === 0 || inputCelular.length === 0) {
+    if (!senhaVerifica || !cpfVerifica || inputNome.length === 0 || inputDataNascimento.length === 0 || inputEmail.length === 0 || inputSenha.length === 0 || inputCelular.length === 0) {
         msgErrorCampo.setAttribute('style', 'display:block');
         msgErrorCheck.setAttribute('style', 'display:none');
     }
@@ -196,18 +196,7 @@ formulario.addEventListener('submit', function(event) {
         checkboxLabel.setAttribute('style', 'color: #006494');
         msgErrorCampo.setAttribute('style', 'display:none');
         msgErrorCheck.setAttribute('style', 'display:none');
-        //cadastrar();
-        const informacoes =            
-        {nome: inputNome.value,
-        email: inputEmail.value,
-        senha: inputSenha.value,
-        numero: inputCelular.value,
-        cpf: inputCpf.value,
-        nascimento: inputDataNacimento.value
-        }
         limpar();
-        console.log(informacoes);
-
     }
     
 
@@ -228,16 +217,16 @@ function cadastrar(){
             senha: inputSenha.value,
             numero: inputCelular.value,
             cpf: inputCpf.value,
-            nascimento: inputDataNacimento.value
+            nascimento: inputDataNascimento.value
         })
     })
     .then(function (res) { console.log(res)})
-    .catch(function (res) { console.log(res)})
+    .catch(function (error) { console.log(error)})
 };
 
 function limpar(){
     inputNome.value = "";
-    inputDataNacimento.value = "";
+    inputDataNascimento.value = "";
     inputCpf.value = "";
     inputCelular.value = "";
     inputEmail.value = "";
